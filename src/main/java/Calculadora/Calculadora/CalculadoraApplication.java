3991723b857fdbe29fba90dcf599b208731ffa58
Ingridols35 <ingridols35@gmail.com>
2023-07-31 19:03:10 -0300

    first commit

--- src/main/java/Calculadora/Calculadora/CalculadoraApplication.java
+++ src/main/java/Calculadora/Calculadora/CalculadoraApplication.java
@@ -11,10 +11,41 @@ public class CalculadoraApplication {
 
 	public static void main(String[] args) {
 		JFrame jframe = new JFrame();
+
+		String operador = JOptionPane.showInputDialog("+    -   *   /")         ;
 		int n1 = Integer.parseInt(JOptionPane.showInputDialog("Insira um valor"));
 		int n2 = Integer.parseInt(JOptionPane.showInputDialog("Insira um valor"));
-		var soma = n1 + n2;
-		JOptionPane.showConfirmDialog(jframe, "Resultado: "+ soma)        ;
+	
+
+
+		
+		if(operador.equals("+")){
+			     	var soma = n1 + n2;
+			             		JOptionPane.showConfirmDialog(jframe, "Resultado: "+ soma)     ;
+		}
+
+
+						 if(operador.equals("-")){
+		           	     	var subtracao = n1 - n2;
+		           	             		JOptionPane.showConfirmDialog(jframe, "Resultado: "+ subtracao)     ;
+		           }
+
+
+		         if(operador.equals("*")){
+				 	     	var multiplicacao = n1 * n2;
+				 	             		JOptionPane.showConfirmDialog(jframe, "Resultado: "+ multiplicacao) ;    ;
+
+				 }
+
+
+
+  if(operador.equals("/")){
+		var divisao = n1 / n2;
+			JOptionPane.showConfirmDialog(jframe, "Resultado: "+ divisao)     ;
+				                   }
+
+
+
 	}
 
 }
